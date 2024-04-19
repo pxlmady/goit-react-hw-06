56a5a8380ee6f4de12015e7a563e4a892f9cadc2
pxlmady <yoplufre@gmail.com>
2024-04-19 17:51:41 +0300

    nesting fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "../src/components/App/App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { persistor, store } from "./components/redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
